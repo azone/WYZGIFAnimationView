39e36ad6374fb8a9e238fbd867341a62f8fe52fc
Yozone Wong <wangyaozh@gmail.com>
2014-01-03 22:39:35 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Yozone Wang on 14-1-3.
+//  Copyright (c) 2014年 Yozone Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Yozone Wang on 14-1-3.
+//  Copyright (c) 2014年 Yozone Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
